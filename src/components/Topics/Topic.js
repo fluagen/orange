29275b1800d947ddf1490877b7cf7a5ed9cfc13b
majie <majie@calis.edu.cn>
2018-03-23 00:12:33 +0800

    update topic component

--- src/components/Topics/Topic.js
+++ src/components/Topics/Topic.js
@@ -1,31 +1,45 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'antd';
-import About from '../Aside/About';
-import Replies from '../Reply/Replies';
-import { RequestGet } from '../Util/Request';
+import React, { Component } from "react";
+import { Row, Col } from "antd";
+import About from "../Aside/About";
+import Replies from "../Reply/Replies";
+import { RequestGet } from "../Util/Request";
 
-import styles from './Topic.module.scss';
+import styles from "./Topic.module.scss";
 
 class Topic extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      topic:{}
+      tid: props.match.params.tid,
+      topic: {}
     };
+    this.load = this.load.bind(this);
   }
-  componentDidMount() {
-    console.log('Topic componentDidMount');
-    let tid = this.props.match.params.tid;
-    let url = 'http://localhost:3000/public/topic/' + tid;
+  load(url) {
     RequestGet(url, rst => {
       let topic = rst.data.topic;
-      this.topic;
       this.setState({
+        tid: topic._id,
         topic: topic
       });
+      // console.log(this.state.topic)
     });
-    console.log(tid);
   }
+  componentDidMount() {
+    let tid = this.props.match.params.tid;
+    let url = "http://localhost:3000/public/topic/" + tid;
+    this.load(url);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let tid = this.state.tid;
+    if (tid != nextProps.match.params.tid) {
+      tid = nextProps.match.params.tid;
+      let url = "http://localhost:3000/public/topic/" + tid;
+      this.load(url);
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -35,7 +49,9 @@ class Topic extends Component {
               <div className="list-group-item">
                 <h1 className={styles.title}>{this.state.topic.title}</h1>
               </div>
-              <div className="list-group-item">{this.state.topic.t_content}</div>
+              <div className="list-group-item">
+                {this.state.topic.t_content}
+              </div>
               <div className="list-group-item">工具栏</div>
             </div>
             <Replies />
